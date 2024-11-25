c769650d10d0dc5b2c2851bbbfd730d286e17da3
Mark Hamilton <mark.lee.hamilton@gmail.com>
2024-11-25 09:55:07 -0800

    emacs configuration

---
+++ emacs.d/init.el
@@ -0,0 +1,7 @@
+(global-set-key (kbd "C-c c") 'compile)
+(global-set-key (kbd "C-c n") 'next-error)
+(global-set-key (kbd "C-c p") 'prev-error)
+(global-set-key (kbd "C-c k") 'kill-compilation)
+
+(setq viper-mode t)
+(viper-mode)
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p ~/.emacs.d/
+cp -u emacs.d/init.el ~/.emacs.d/init.el
