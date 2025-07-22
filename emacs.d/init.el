6ec1ab67817e5926d5d251a93727e7f52d06469b
Mark Hamilton <mark.lee.hamilton@gmail.com>
2025-07-22 00:29:31 +0000

    add copilot

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,7 +1,17 @@
+(require 'package)
+
+(setq viper-mode t)
+(viper-mode)
+
 (global-set-key (kbd "C-c c") 'compile)
 (global-set-key (kbd "C-c n") 'next-error)
 (global-set-key (kbd "C-c p") 'prev-error)
 (global-set-key (kbd "C-c k") 'kill-compilation)
 
-(setq viper-mode t)
-(viper-mode)
+
+(copilot-mode)
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
+
+(global-set-key (kbd "<tab>") 'copilot-accept-completion)
+(global-set-key (kbd "TAB") 'copilot-accept-completion)
